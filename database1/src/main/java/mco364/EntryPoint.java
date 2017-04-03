bb5c0522e8b4ac57b7fe24466611f9c1482483ab
profyoni <jrobinson@touro.edu>
2017-04-03 17:50:28 -0400

    Merge origin/master
    Conflicts:
    	Royalmess/src/main/java/mco364/EntryPoint.java

--- database1/src/main/java/mco364/EntryPoint.java
+++ database1/src/main/java/mco364/EntryPoint.java
@@ -6,11 +6,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+// simple stuff
+// in SSMS create a table
+// In java display table, allow user to update, delete, add data to table
+
+// more complex : persist free style na do other images to a database
 public class EntryPoint {
 //https://docs.oracle.com/javase/tutorial/jdbc/
     // add gradle dependency
+
     public static void main(String[] args) throws SQLException {
-        try{
+        try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             System.out.println("Driver Successfully Loaded!");
             String driver = "jdbc:sqlserver:";
@@ -19,25 +25,23 @@ public class EntryPoint {
             String username = "DS";
             String password = "";
             String database = "DS";
-            String connection = driver + url + port + 
-   ";databaseName=" + database +";user=" + username + ";password=" + password + ";";  
+            String connection = driver + url + port
+                    + ";databaseName=" + database + ";user=" + username + ";password=" + password + ";";
             try (Connection connect = DriverManager.getConnection(connection)) {
                 System.out.println("Connected to Database!");
                 PreparedStatement state = connect.prepareStatement("Select * from mytable");
                 System.out.println("Query Executed Successfully!");
                 ResultSet rs = state.executeQuery();
-while (rs.next()) {
-        System.out.println(rs.getString("id"));
-        System.out.println(rs.getString("name"));
-    }
+                while (rs.next()) {
+                    System.out.println(rs.getString("id"));
+                    System.out.println(rs.getString("name"));
+                }
             }
             System.out.println("Database Closed!");
-        }
-        catch(ClassNotFoundException ex){
+        } catch (ClassNotFoundException ex) {
             System.out.println("Error: Driver Class not found.");
             ex.printStackTrace();
-        }
-        catch(SQLException sqlex){
+        } catch (SQLException sqlex) {
             System.out.println("Error: SQL Error");
             sqlex.printStackTrace();
 
